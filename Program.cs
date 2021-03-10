3ac35a5ee5935eb724bffb160f3ec43f5a5c22d7
Takacs Robert <89.t.robert@gmail.com>
2021-03-10 11:28:18 +0100

    feat(Heap): added example of heap sorting algorithm

--- Program.cs
+++ Program.cs
@@ -7,5 +7,22 @@ namespace Data_Structures
         private static void Main(string[] args)
         {
         }
+
+        private static void HeapSort()
+        {
+            int[] numbers = { 45, 30, 67, 11, 13, 58, 104, 66, 51 };
+            var heap = new Heap.Heap(numbers.Length);
+
+            foreach (int number in numbers)
+            {
+                heap.Insert(number);
+            }
+
+            for (var i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = heap.Remove();
+                Console.WriteLine(numbers[i]);
+            }
+        }
     }
 }
