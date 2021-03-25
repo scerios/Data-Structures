7d5ac8e226fa379ac6161920583ec7ee81535b0a
Takacs Robert <89.t.robert@gmail.com>
2021-03-25 09:36:16 +0100

    feat(Sorter): added solution or BubbleSort

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Data_Structures.Sorting;
+using System;
 
 namespace Data_Structures
 {
@@ -6,6 +7,15 @@ namespace Data_Structures
     {
         private static void Main(string[] args)
         {
+            var sorter = new Sorter();
+
+            var numbers = new int[] { 85, 3, 26, 4, 11, 53, 7, 20, 12, 5, 36 };
+            sorter.Bubble(numbers);
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
---
+++ Sorting/Sorter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Data_Structures.Sorting
+{
+    public class Sorter
+    {
+        public int[] Bubble(int[] numbers)
+        {
+            var index = 0;
+            var counter = 0;
+
+            while (numbers.Length - 1 - counter > 0)
+            {
+                if (numbers[index] > numbers[index + 1])
+                {
+                    Swap(numbers, index, index + 1);
+                }
+
+                index++;
+
+                if (index == numbers.Length - 1 - counter)
+                {
+                    index = 0;
+                    counter++;
+                }
+            }
+
+            return numbers;
+        }
+
+        private void Swap(int[] numbers, int i, int j)
+        {
+            var temp = numbers[j];
+            numbers[j] = numbers[i];
+            numbers[i] = temp;
+        }
+    }
+}
